5531d0ee3d1f4700b5264804af2d67376a338843
vancook <237904851@qq.com>
2017-05-10 11:51:19 +0800

    fix bug

--- usms-server/src/main/java/net/evecom/common/usms/uma/controller/PrivilegeAPI.java
+++ usms-server/src/main/java/net/evecom/common/usms/uma/controller/PrivilegeAPI.java
@@ -6,13 +6,11 @@
 package net.evecom.common.usms.uma.controller;
 
 import net.evecom.common.usms.core.model.ErrorStatus;
-import net.evecom.common.usms.entity.OperationEntity;
 import net.evecom.common.usms.entity.PrivilegeEntity;
 import net.evecom.common.usms.entity.UserEntity;
 import net.evecom.common.usms.oauth2.Constants;
 import net.evecom.common.usms.oauth2.service.OAuthService;
 import net.evecom.common.usms.uma.service.PrivilegeService;
-import net.evecom.common.usms.uma.service.StaffService;
 import net.evecom.common.usms.uma.service.UserService;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -27,7 +25,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -45,6 +42,7 @@ public class PrivilegeAPI {
     /**
      * 注入PrivilegeService
      */
+    @Autowired
     private PrivilegeService privilegeService;
 
     /**
@@ -59,47 +57,43 @@ public class PrivilegeAPI {
     @Autowired
     private UserService userService;
 
+
     /**
-     * 注入StaffService
+     * 判断是否拥有该权限
+     *
+     * @param request
+     * @return ResponseEntity
      */
-    @Autowired
-    private StaffService staffService;
-
-
-
-   /**
-    * 判断是否拥有该权限
-    * @return ResponseEntity
-    * @param request
-    */
     @RequestMapping(value = "privilege/exist", produces = "application/json; charset=UTF-8")
-    public ResponseEntity getPrivilege(HttpServletRequest request) throws OAuthProblemException, OAuthSystemException {
+    public ResponseEntity getPrivilege(HttpServletRequest request)
+            throws OAuthProblemException, OAuthSystemException {
         String privilegeName = request.getParameter("privilege");
-       if(StringUtils.isEmpty(privilegeName)){
-           ErrorStatus errorStatus = new ErrorStatus
-                   .Builder(ErrorStatus.INVALID_PARAMS, Constants.INVALID_PARAMS)
-                   .buildJSONMessage();
-           return new ResponseEntity(errorStatus.getBody(), HttpStatus.BAD_REQUEST);
-       }
-       // 构建OAuth资源请求
-       OAuthAccessResourceRequest oauthRequest = new OAuthAccessResourceRequest(request, ParameterStyle.QUERY);
-       // 获取Access Token
-       String accessToken = oauthRequest.getAccessToken();
-       // 获取用户名
-       String loginName = oAuthService.getLoginNameByAccessToken(accessToken);
-       // 获得用户实体类
-       UserEntity user = userService.findByLoginName(loginName);
-       JSONObject jsonObject = new JSONObject();
-       if (privilegeService.hasPrivilege(user.getId(), privilegeName)) {
-           jsonObject.put("result", true);
-       } else jsonObject.put("result", false);
-       return new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
-   }
+        if (StringUtils.isEmpty(privilegeName)) {
+            ErrorStatus errorStatus = new ErrorStatus
+                    .Builder(ErrorStatus.INVALID_PARAMS, Constants.INVALID_PARAMS)
+                    .buildJSONMessage();
+            return new ResponseEntity(errorStatus.getBody(), HttpStatus.BAD_REQUEST);
+        }
+        // 构建OAuth资源请求
+        OAuthAccessResourceRequest oauthRequest = new OAuthAccessResourceRequest(request, ParameterStyle.QUERY);
+        // 获取Access Token
+        String accessToken = oauthRequest.getAccessToken();
+        // 获取用户名
+        String loginName = oAuthService.getLoginNameByAccessToken(accessToken);
+        // 获得用户实体类
+        UserEntity user = userService.findByLoginName(loginName);
+        JSONObject jsonObject = new JSONObject();
+        if (privilegeService.hasPrivilege(user.getId(), privilegeName)) {
+            jsonObject.put("result", true);
+        } else jsonObject.put("result", false);
+        return new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
+    }
 
     /**
      * 获取权限列表
-     * @return ResponseEntity
+     *
      * @param request
+     * @return ResponseEntity
      */
     @RequestMapping(value = "privileges", produces = "application/json; charset=UTF-8")
     public ResponseEntity getPrivileges(HttpServletRequest request) {
@@ -124,4 +118,5 @@ public class PrivilegeAPI {
         jsonObject.put("privileges", priJsonArr);
         return new ResponseEntity(jsonObject.toString(), HttpStatus.OK);
     }
+
 }
